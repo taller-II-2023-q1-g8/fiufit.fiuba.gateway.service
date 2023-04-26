089734221d8fa44ecb12129b2951446e020b2343
Andy <amoyano@fi.uba.ar>
2023-04-26 00:23:21 -0300

    Express Swagger Doc

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 const cors = require('cors');
 const user_route = require("./routes/users_routes")
+const setupSwagger = require('./middleware/express-jsdoc-swagger');
 const cors_options = {
   origin: "*"
 }
@@ -10,7 +11,7 @@ const cors_options = {
 var app = express();
 const port = 3000;
 
-
+setupSwagger(app);
 
 app.use(logger('dev'));
 app.use(cors(cors_options));
@@ -22,11 +23,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routers
 app.use('/user', user_route.router)
 
+/**
+ * GET /
+ * @summary Hola Mundo
+ * @security basicAuth
+ * @tags Microservicios
+ * @return {object} 200 - Éxito
+ * @return {object} 403 - Error
+ * @example response - 200 - Éxito
+ * [
+ *   {
+ *     "message": "Hello World!"
+ *   }
+ * ]
+ */
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World!'
   })
 })
+
 app.listen(port, () => {
     console.log(`Gateway listening on port ${port}`)
 })
