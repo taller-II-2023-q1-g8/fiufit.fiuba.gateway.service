82e594b42829ad7de7a7fa44054074fcf67e8a29
Andy <amoyano@fi.uba.ar>
2023-07-04 19:13:29 -0300

    import fix

--- routes/goals_and_metrics_routes.js
+++ routes/goals_and_metrics_routes.js
@@ -4,20 +4,13 @@ const router = express.Router()
 //const checkAuth = auth_middleware.checkAuth
 const axios = require('axios')
 
-const url_metrics_and_goals = process.env.URL_METRICS_AND_GOALS;
+import { validateApiKey } from "../middleware/api_key_validation.js"
 
+const url_metrics_and_goals = process.env.URL_METRICS_AND_GOALS;
 if (url_metrics_and_goals == null){
     console.log("No URL found for Metrics and Goals Microservice in Environment Variables.")
     process.exit(-1)
 }
-
-const url_services = process.env.URL_SERVICES;
-if (url_services == null){
-    console.log("No URL found for Service Manager Microservice in Environment Variables.")
-    process.exit(-1)
-}
-
-import { validateApiKey } from "../middleware/api_key_validation.js"
 
 router.all('*', async function(req, res) {
     const apiKey = req.header('FiuFitAuth');
